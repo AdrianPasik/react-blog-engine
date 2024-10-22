9f3abeb756cffe9dc5f3ca34ab7c48e742dfd5cf
Adrian Pasik <adrian.pasik@vp.pl>
2024-10-22 16:48:04 +0200

    nav scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './navigation/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar />
     <h1 className="text-3xl font-bold underline">
       Tailwind test
     </h1>
